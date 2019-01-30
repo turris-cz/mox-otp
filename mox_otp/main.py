923ea66571e21d224e55c9e4fea730d6cbaae08e
Vojtech Myslivec <vojtech.myslivec@nic.cz>
2019-01-31 00:46:42 +0100

    Split script into functions

--- mox_otp/main.py
+++ mox_otp/main.py
@@ -9,6 +9,13 @@ SYSFS_ROOT = "/sys/devices/platform/soc/soc:internal-regs@d0000000/soc:internal-
 PUBKEY_PATH = SYSFS_ROOT + "mox_pubkey"
 SIGN_PATH = SYSFS_ROOT + "mox_do_sign"
 
+# max number of bytes to read from sysfs sig file
+MAX_SIGNATURE_LENGTH = 512
+
+
+def errprint(*args, **kwargs):
+    print(*args, **kwargs, file=sys.stderr)
+
 
 def change_endian(s):
     res = b''
@@ -17,39 +24,45 @@ def change_endian(s):
     return res
 
 
+def check_pubkey():
+    try:
+        with open(PUBKEY_PATH, "r") as f:
+            pubkey = f.readline()
+    except (FileNotFoundError, PermissionError):
+        errprint("Could not find MOX pubkey file (probably not running on MOX device)")
+        exit(2)
+
+    if pubkey in ["", "\n", "none\n"]:
+        errprint("This device does not have its OTP key generated or accessible")
+        exit(2)
+
+
+def sign_message(message):
+    h = sha512()
+    h.update(bytes(message, encoding="utf-8"))
+    dig = h.digest()
+
+    try:
+        with open(SIGN_PATH, "wb") as s:
+            s.write(change_endian(dig))
+        with open(SIGN_PATH, "rb") as s:
+            sig = change_endian(s.read(MAX_SIGNATURE_LENGTH))
+    except (FileNotFoundError, PermissionError):
+        errprint("Could not find MOX sign file – the sysfs API is probably broken")
+        exit(3)
+
+    print((sig[2:68] + sig[70:]).hex())
+
+
 def main():
-    pubkey = open(PUBKEY_PATH).read()
-
-    if pubkey == "none\n":
-        print("no public key burned")
-        exit(1)
-
-    print("MOX burned public key: {}".format(pubkey))
+    check_pubkey()
 
     if len(sys.argv) < 2:
         print("message not given")
         exit(1)
 
     message = sys.argv[1]
-    print("message: {}".format(message))
-
-    h = sha512()
-    h.update(bytes(message, encoding='utf-8'))
-    dig = h.digest()
-
-    print("message hash: {}".format(h.hexdigest()))
-
-    s = open(SIGN_PATH, "wb")
-    s.write(change_endian(dig))
-    s.close()
-
-    s = open(SIGN_PATH, "rb")
-    sig = s.read()
-    s.close()
-
-    sig = change_endian(sig)
-
-    print("signature: {}".format((sig[2:68] + sig[70:]).hex()))
+    sign_message(message)
 
 
 if __name__ == "__main__":
